b940368d93d4b1040604c34f754821ac47c05452
ojzene <ttconfirmed@gmail.com>
2018-03-20 05:23:33 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -24,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private iab: InAppBrowser, public loadingController:LoadingController) {}
+  ngOnInit(){
+
+    // let loading = this.loadingController.create({content : "Loading Kanmit Site, please wait..."});
+    // loading.present();
+    // const browser = this.iab.create('https://www.kanmittechnologies.ng/','_self',{location:'no'});
+    // if(browser) {
+    //   loading.dismissAll();
+    // }
+
+    this.ionViewLoaded();
 
   }
 
+  ionViewLoaded() {
+    let loader = this.loadingController.create({ content: 'Loading Kanmit Site, please wait...', });
+
+    loader.present().then(() => {
+      this.iab.create('https://www.kanmittechnologies.com/','_self',{location:'no'});
+      loader.dismiss();
+    });
+  }
 }
